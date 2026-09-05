cb033b1fcb390c6bb1269e374e4976099c77e868
gustavolima22-arch <gustavo.francisco@aluno.impacta.edu.br>
2026-09-04 21:06:12 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,19 @@
+Nome = "Gustavo"
+print (Nome)
+
+22102 - 12997 * 330
+
+mensagem = "Ola, Gustavo!"
+print (mensagem)
+
+matematica = 22102 - 12997 * 330
+
+print (matematica)
+texto = "22102 - 12997 * 330"
+print (texto)
+
+expressao = texto +  " = " + str(matematica)
+print(expressao)
+
+expressao1 = f"{texto} = {matematica}"
+print (expressao1)
